9667c505bda752b951c75757facfd6a1581c59b9
oksongh <oksongh@gmail.com>
2025-07-27 02:51:11 +0900

    add fail case

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 fn unagi_check(s: &str) -> bool {
     s.contains("unagi")
 }
+
+fn fish_check(s: &str) -> bool {
+    s.contains("saba") || s.contains("hugu")
+}
 fn main() {
     unagi_check("unagi");
     println!("Hello, world!");
 }
 #[test]
-fn test_ci_desu() {
+fn test_ci_pass_desu() {
     assert!(unagi_check("unagi"));
     assert!(unagi_check("may unagi"));
     assert!(unagi_check("unagi is here"));
 
     assert!(!unagi_check("una"));
+}
+#[test]
+fn test_ci_fail_desu() {
+    assert!(fish_check("saba"));
+    assert!(fish_check("hugu"));
+    assert!(fish_check("saba and hugu"));
+    assert!(fish_check("unagi"));
+    assert!(fish_check("una"));
+
+    assert!(fish_check("usagi"));
 }
